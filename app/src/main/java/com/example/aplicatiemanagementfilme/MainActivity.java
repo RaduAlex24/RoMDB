2eee6336b16bf49daaa40b333d4fa5e46d4288bc
RaduAlex24 <radualexandru18@stud.ase.ro>
2020-12-24 17:57:45 +0200

    Log in nad sign up fara db

--- app/src/main/java/com/example/aplicatiemanagementfilme/MainActivity.java
+++ app/src/main/java/com/example/aplicatiemanagementfilme/MainActivity.java
@@ -1,19 +1,82 @@
 package com.example.aplicatiemanagementfilme;
 
+import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.CheckBox;
+import android.widget.Toast;
+
+import com.example.aplicatiemanagementfilme.util.UserAccount;
+import com.google.android.material.textfield.TextInputEditText;
 
 public class MainActivity extends AppCompatActivity {
 
-    int a=2;
-    int b=2;
-    int c=2;
-    int d=2;
+    public static final int SIGN_UP_REQUEST_CODE = 200;
+    private TextInputEditText tiet_username;
+    private TextInputEditText tiet_password;
+    private CheckBox checkBoxRemember;
+    private Button btn_login;
+    private Button btn_signup;
+
+    private UserAccount userAccount = null;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        // Initializare componente APELARE
+        initComponents();
+
+        // Atasare functie buton pt start activity Signup APELARE
+        btn_signup.setOnClickListener(onClickOpenSignUpListener());
+    }
+
+
+    // Activity result
+
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, @Nullable Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+
+        // Primire userAccount nou
+        if(requestCode == SIGN_UP_REQUEST_CODE && resultCode == RESULT_OK && data!=null){
+            userAccount = (UserAccount) data.getSerializableExtra(SignUpActivity.USER_ACCOUNT_KEY);
+            // Populare log in
+            populateLogIn(userAccount);
+        }
+    }
+
+
+    // Functii
+    // Initializare componente
+    private void initComponents() {
+        tiet_username = findViewById(R.id.tiet_username_login);
+        tiet_password = findViewById(R.id.tiet_password_login);
+        checkBoxRemember = findViewById(R.id.checkbox_remember_login);
+        btn_login = findViewById(R.id.btn_login_login);
+        btn_signup = findViewById(R.id.btn_signup_login);
+    }
+
+    // Functie buton pt start activity sign up
+    private View.OnClickListener onClickOpenSignUpListener() {
+        return new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(getApplicationContext(), SignUpActivity.class);
+                startActivityForResult(intent, SIGN_UP_REQUEST_CODE);
+            }
+        };
+    }
+
+    // Populare campuri log in
+    private void populateLogIn(UserAccount userAccount){
+        tiet_username.setText(userAccount.getUsername());
+        tiet_password.setText(userAccount.getPassword());
     }
 }
---
+++ app/src/main/java/com/example/aplicatiemanagementfilme/SignUpActivity.java
@@ -0,0 +1,119 @@
+package com.example.aplicatiemanagementfilme;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.accounts.Account;
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.Toast;
+
+import com.example.aplicatiemanagementfilme.util.UserAccount;
+import com.google.android.material.textfield.TextInputEditText;
+
+public class SignUpActivity extends AppCompatActivity {
+
+    public static final String USER_ACCOUNT_KEY = "USER_ACCOUNT_KEY";
+    private TextInputEditText tiet_username;
+    private TextInputEditText tiet_password;
+    private TextInputEditText tiet_email;
+    private TextInputEditText tiet_fullname;
+    private Button btn_signup;
+
+    private Intent intent;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_sign_up);
+
+        // Initializare componente APELARE
+        initComponents();
+
+        // Atasare functie buton pt Signup APELARE
+        btn_signup.setOnClickListener(onClickSignUpListener());
+    }
+
+
+    // Functii
+    // Initializare componente
+    private void initComponents() {
+        tiet_username = findViewById(R.id.tiet_username_signup);
+        tiet_password = findViewById(R.id.tiet_password_signup);
+        tiet_email = findViewById(R.id.tiet_email_signup);
+        tiet_fullname = findViewById(R.id.tiet_fullname_signup);
+        btn_signup = findViewById(R.id.btn_signup_signup);
+
+        // Preluare intent
+        intent = getIntent();
+    }
+
+
+    // Atasare functie buton pt Signup
+    private View.OnClickListener onClickSignUpListener() {
+        return new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                if (validate()) {
+                    // validare db
+                    UserAccount userAccount = createAccountFromWidgets();
+                    intent.putExtra(USER_ACCOUNT_KEY, userAccount);
+                    setResult(RESULT_OK, intent);
+                    finish();
+                }
+            }
+        };
+    }
+
+
+    // Functie validare componente fara db
+    private boolean validate() {
+        if (tiet_username.getText().toString().trim().length() <= 5
+                || tiet_username.getText().toString().trim().length() >= 16) {
+            Toast.makeText(getApplicationContext(),
+                    getString(R.string.toast_username_signup),
+                    Toast.LENGTH_SHORT).show();
+            return false;
+        }
+
+        if (tiet_password.getText().toString().trim().length() <= 5
+                || tiet_password.getText().toString().trim().length() >= 16) {
+            Toast.makeText(getApplicationContext(),
+                    getString(R.string.toast_password_signup),
+                    Toast.LENGTH_SHORT).show();
+            return false;
+        }
+
+        // Bonus validare adresa email cu @ si .
+        if (tiet_email.getText().toString().trim().length() <= 9
+                || tiet_email.getText().toString().trim().length() >= 26) {
+            Toast.makeText(getApplicationContext(),
+                    getString(R.string.toast_email_signup),
+                    Toast.LENGTH_SHORT).show();
+            return false;
+        }
+
+        if (tiet_fullname.getText().toString().trim().length() <= 4
+                || tiet_fullname.getText().toString().trim().length() >= 26) {
+            Toast.makeText(getApplicationContext(),
+                    getString(R.string.toast_fullname_signup),
+                    Toast.LENGTH_SHORT).show();
+            return false;
+        }
+
+
+        return true;
+    }
+
+
+    // Creare account din widgets
+    private UserAccount createAccountFromWidgets() {
+        String username = tiet_username.getText().toString();
+        String password = tiet_password.getText().toString();
+        String email = tiet_email.getText().toString();
+        String fullName = tiet_fullname.getText().toString();
+
+        return new UserAccount(username, password, email, fullName);
+    }
+}
